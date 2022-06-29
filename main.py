78ed94f36f8f1ffad2383188f6e4d4da8a092174
suisse <romain.bedert@gmail.com>
2022-06-29 13:19:12 +0200

    modif pour test fonctionnement = Résultat ok! récpa : on recoit le nom du fichier en argument Il faut faire les modifications sur ce fichier puis enregistrer On répond avec un xml contenant le nom du fichier traiter et le status Le nom du fichier xml doit etre le nom de l'exe Ensuite Teexma récupére ce fichier et le converti au format de sortie désiré

--- main.py
+++ main.py
@@ -3,13 +3,26 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import sys
+import os
+import xml.etree.cElementTree as ET
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(sys.argv)
-    f = open("output.txt", "w")
-    f.write(str(sys.argv))
-    f.close()
+    #print('test')
+    #print(sys.argv)
+    filepath = sys.argv[1]
+
+    dir = os.path.dirname(filepath)
+
+    root = ET.Element("Output")
+    ET.SubElement(root, "Status").text = "OK"
+    ET.SubElement(root, "OutputFilePath").text = filepath
+
+    tree = ET.ElementTree(root)
+    ET.indent(tree, space="\t", level=0)
+    tree.write(dir+"\main.xml", encoding="utf-8", xml_declaration=True)
+
 
 
